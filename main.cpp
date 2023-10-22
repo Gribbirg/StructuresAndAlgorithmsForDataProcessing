ac2c1efc926b05ae092dcbac44cfa5f6a1b2c35d
Alexander Gribkov <gribkovalexander@gmail.com>
2023-10-23 00:07:41 +0300

    Practice 4 final v13

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Practice_1/Pr1.h"
+#include "Practice_2/Practice2.h"
 #include "Practice_3/Practice3.h"
 #include "Practice_4/Practice4.h"
 
@@ -8,6 +10,8 @@ int main() {
     cout << "Structures and algorithms for data processing variant 3" << endl;
     cout << endl;
 
+//    Pr1::start();
+//    Practice2::start();
 //    Practice3::start();
     Practice4::start();
 }
